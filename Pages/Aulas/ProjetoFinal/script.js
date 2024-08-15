eae2e14716359f5e300d94bf28a473de3518355d
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-08-15 15:56:43 -0300

    Inicio da lógica do JS

---
+++ Pages/Aulas/ProjetoFinal/script.js
@@ -0,0 +1,9 @@
+
+let cartQuantity = 0;
+
+const cartQuantityElement = document.getElementById('cart-quantity');
+const addToCartButton = document.getElementById('btn-add');
+const addFormToCart = document.getElementById('add-to-cart-form');
+
+
+
